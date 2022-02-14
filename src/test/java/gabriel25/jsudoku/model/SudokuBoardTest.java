367e5770f20f5e68f920b1b0161ccc2c96cb3338
Gabriel025 <barnoth.g@gmail.com>
2022-02-14 02:36:55 +0100

    Test class for SudokuBoard (empty for now)

---
+++ src/test/java/gabriel25/jsudoku/model/SudokuBoardTest.java
@@ -0,0 +1,20 @@
+package gabriel25.jsudoku.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SudokuBoardTest {
+    /*
+    SudokuBoard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new SudokuBoard();
+    }
+
+    @Test
+    void setSingleCell() {
+
+    }
+    */
+}
