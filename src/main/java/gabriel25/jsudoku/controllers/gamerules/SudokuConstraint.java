693a95939d260da25f1f4593dcce877c16211330
Gabriel025 <barnoth.g@gmail.com>
2022-02-14 02:18:39 +0100

    Better class names for rules and game variants

---
+++ src/main/java/gabriel25/jsudoku/controllers/gamerules/SudokuConstraint.java
@@ -0,0 +1,6 @@
+package gabriel25.jsudoku.controllers.gamerules;
+
+/** Strategy class describing a single constraint */
+public abstract class SudokuConstraint {
+
+}
---
+++ src/main/java/gabriel25/jsudoku/controllers/gamerules/SudokuVariant.java
@@ -0,0 +1,5 @@
+package gabriel25.jsudoku.controllers.gamerules;
+
+public abstract class SudokuVariant {
+    
+}
