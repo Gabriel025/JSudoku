5a25adb57b379f6571a6cd41253da8d0d9b2ef7a
Gabriel025 <barnoth.g@gmail.com>
2022-02-14 02:36:28 +0100

    SudokuBoard initial implementation

--- src/main/java/gabriel25/jsudoku/model/SudokuBoard.java
+++ src/main/java/gabriel25/jsudoku/model/SudokuBoard.java
@@ -1,13 +1,13 @@
 package gabriel25.jsudoku.model;
 
-import java.util.ArrayList;
-import java.util.EventListener;
-import java.util.EventObject;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 
 public class SudokuBoard {
+    /**
+     * Inner class that is used for easier manipulation of the board.
+    */
     public class Cell {
         private int row, col;
         
@@ -16,69 +16,107 @@ public class SudokuBoard {
             this.col = col;
         }
 
-        public int getValue() { return SudokuBoard.this.grid[row][col]; }
-        public void setValue(int val) { this.val = val; }
+        public int getValue() { return SudokuBoard.this.values[row][col]; }
+        public void setValue(int val) { SudokuBoard.this.values[row][col] = val; }
 
-        public int[] getPencilMarks() { return new int[]{}; }
-        public void setPencilMark(int val) { }
-        public void clearPencilMark(int val) { }
-        public void togglePencilMark(int val) { }
-        public void setPencilMark(int[] val) { }
+        public boolean isPencilMarkSet(int digit) {
+            int pm[] = SudokuBoard.this.pencilMarks[row][col];
+
+            return Arrays.stream(pm).anyMatch((int i) -> i == digit);
+        }
+
+        public int[] getPencilMarks() {
+            int pm[] = SudokuBoard.this.pencilMarks[row][col];
+
+            return Arrays.stream(pm).filter((int i) -> i != 0).toArray();
+        }
+
+        public void setPencilMark(int val) {
+            int pm[] = SudokuBoard.this.pencilMarks[row][col];
+
+            if(pm[0] != 0) return; //all pencil marks are already set
+            
+            pm[0] = val;
+            Arrays.sort(pm);
+        }
+
+        public void erasePencilMark(int val) {
+            int pm[] = SudokuBoard.this.pencilMarks[row][col];
+
+            pm = Arrays.stream(pm)
+                       .map((int i) -> i == val ? 0 : i)
+                       .sorted().toArray();
+        }
+
+        public void togglePencilMark(int val) {
+            if(isPencilMarkSet(val))
+                erasePencilMark(val);
+            else
+                setPencilMark(val);
+        }
+
+        public void overridePencilMarks(int[] vals) {
+            int pm[] = SudokuBoard.this.pencilMarks[row][col];
+            
+            pm = Arrays.copyOf(vals, 9);
+            Arrays.sort(pm);
+        }
+
+        public void clearPencilMarks() {
+            SudokuBoard.this.pencilMarks[row][col] =
+                IntStream.generate(() -> 0).limit(9).toArray();
+        }
     }
 
-    private ConcurrentLinkedQueue<Listener> listeners;
-
-    private Cell grid[][];
+    //later on: differentiate between given values (clues) and user values
+    //also possibly include the solution as another array (not neccesary though)
+    private int values[][];
+    private int pencilMarks[][][];
 
     public SudokuBoard() {
-        grid = new Cell[9][9];
+        //these 9's won't be hardcoded and grid size will be sourced from SudokuVariants, once implemented
+        //as a result, to avoid future refactoring, I'm not using field initializers for these arrays
+        values = new int[9][9];
+        //if I really wish to optimize this, I could use bitmaps; for now this works
+        pencilMarks = new int[9][9][9];
+    }
+
+    public SudokuBoard(int givenValues[][]) {
+        this();
+
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++)
+                values[i][j] = givenValues[i][j];
+    }
+
+    public SudokuBoard(SudokuBoard board) {
+        this();
+
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++) {
+                values[i][j] = board.values[i][j];
+                for(int k = 0; k < 9; k++)
+                    pencilMarks[i][j][k] = board.pencilMarks[i][j][k];
+            }
+    }
+
+    public Cell at(int row, int col) {
+        return new Cell(row, col);
+    }
+
+    @Override
+    public int hashCode() {
+        return 73 * Arrays.deepHashCode(values) + 79 * Arrays.deepHashCode(pencilMarks);
+    }
+
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null) return false;
+        if(getClass() != o.getClass()) return false;
         
-        listeners = new ConcurrentLinkedQueue<Listener>();
-    }
-
-    public synchronized Cell at(int row, int col) {
-
-        return grid[row][col];
-    }
-
-    public void addListener(Listener listener) {
-        listeners.add(listener);
-    }
-
-    public void removeListener(Listener listener) {
-        listeners.remove(listener);
+        SudokuBoard cast = (SudokuBoard)o;
+        
+        return Arrays.deepEquals(values, cast.values)
+            && Arrays.deepEquals(pencilMarks, cast.pencilMarks);
     }
 }
-
-
-/*
-
-JSudokuGridView (/possibly rename to editor? pane?)
- -has a reference to a SudokuGrid and observes its changes
- -has events for user input (changing values/pencil marks)
-  and for grid selection (important later for multiplayer stuff)
- -will have a reference to a list of players/sth similar (for names/colors/...)
-
-SudokuGrid
- -2D array of Cell objects
- -has an event for changed values
-
-SudokuClient
- -has a reference to a SudokuGrid object that represents a local copy of the server's state
- -connects to a remote server and synchronizes initial state
- -has sth like get<...>Listener() that returns a listener that can be bound to gridview's events
- -when its listeners are bound, it sends change events and such to the server
- -when it receives changes from the server, it changes the localSudokuGrid object
-   -changes made by the user have to be propagated back by the server in order to take effect (quasi-ACK mechanism)
-
-SudokuServer
- -owns a copy of SudokuGrid that is to be considered the authoritative state of the current game
- -listens for incoming connections and handles them
- -listens for changes posted by active clients, updates SudokuGrid, and broadcasts changes back to clients
- -...
-
-Player
- -name
- -color
- -...
-*/
