d46c1b2aa3940d06c69da3d5999dd47b48dd4b4b
Gabriel025 <barnoth.g@gmail.com>
2022-02-25 16:37:34 +0100

    Adding SudokuGameInstance (encapsulating state)

---
+++ src/main/java/gabriel25/jsudoku/controllers/SudokuGameInstance.java
@@ -0,0 +1,9 @@
+package gabriel25.jsudoku.controllers;
+
+//note: Instance in a class name sounds annoying; but in this case adds clarification
+/**
+ * A class containing all the relevant state of a given game of sudoku.
+ */
+public class SudokuGameInstance {
+    
+}
